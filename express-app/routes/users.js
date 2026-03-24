fde46f42e04d01bcd4f569b341f37bc72c25fc13
AnyaKopytina <kopytinnna@gmail.com>
2026-03-24 19:02:57 +0500

    task 6 done

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -13,6 +13,16 @@ router.get('/', function (req, res, next) {
     });
 })
 
+router.get('/:id', function (req, res, next) {
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId);
+    if (!user) {
+        return res.status(404).json({ message: 'Пользователь не найден' });
+    }
+
+    res.json(user);
+});
+
 router.post('/', function (req, res, next) {
     const {name} = req.body;
 
