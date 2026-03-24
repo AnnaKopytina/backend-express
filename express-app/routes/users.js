f8f2e2c314b8cfd77db192c37395ec7949466df0
AnyaKopytina <kopytinnna@gmail.com>
2026-03-24 18:47:53 +0500

    task 3 done

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
-});
+router.get('/', function (req, res, next) {
+    const members = ['Аня', 'Настя', 'Тима'];
+    res.send(req.json({
+        items: [{id: 1, name: 'Anya'}, {id: 2, name: 'Nastya'}, {id: 3, name: 'Tima'}]
+    }));
+
+
+})
 
 module.exports = router;
